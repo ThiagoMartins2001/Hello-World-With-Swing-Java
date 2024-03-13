1843e52d4bf3a1b535f8a5a5689ff6bd5dcaab2e
Thiago Martins <thiagoestudos2001@gmail.com>
2024-03-12 21:10:29 -0300

    Hello-Word-wih-swing

---
+++ OlaMundoSwing.java
@@ -0,0 +1,94 @@
+import static java.awt.SystemColor.text;
+
+/**
+ *
+ * @author Thiago
+ */
+public class OlaMundoSwing extends javax.swing.JFrame {
+    public OlaMundoSwing() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+                         
+    private void initComponents() {
+
+        lblMensagem = new javax.swing.JLabel();
+        btnClick = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        lblMensagem.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblMensagem.setForeground(new java.awt.Color(204, 0, 0));
+        lblMensagem.setText("Aqui vai aparecer a mensagem");
+
+        btnClick.setText("clique aqui");
+        btnClick.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnClickActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(156, 156, 156)
+                .addComponent(btnClick)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(81, Short.MAX_VALUE)
+                .addComponent(lblMensagem)
+                .addGap(76, 76, 76))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(77, 77, 77)
+                .addComponent(lblMensagem)
+                .addGap(33, 33, 33)
+                .addComponent(btnClick)
+                .addContainerGap(142, Short.MAX_VALUE))
+        );
+
+        pack();
+    }                      
+
+    private void btnClickActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        lblMensagem.setText("Olá, Mundo!");
+    }                                        
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(OlaMundoSwing.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(OlaMundoSwing.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(OlaMundoSwing.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(OlaMundoSwing.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new OlaMundoSwing().setVisible(true);
+            }
+        });
+    }
+                    
+    private javax.swing.JButton btnClick;
+    private javax.swing.JLabel lblMensagem;
+                  
+}
